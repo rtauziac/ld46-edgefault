0491da7923d70934069c553779cec0fbc1c1e0f7
crazyrems <tauziac0remy+fork@gmail.com>
2020-04-19 00:16:42 +0200

    Enemy movement

--- Enemies/EnemyBase.gd
+++ Enemies/EnemyBase.gd
@@ -3,5 +3,10 @@ extends KinematicBody
 export (float) var health = 1
 export (float) var damage = 1
 
+func isAlive():
+	return health > 0
+
 func hit(hitDamage: float):
-	health -= hitDamage
+	if isAlive():
+		health -= hitDamage
+	
