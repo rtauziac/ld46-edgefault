8376671d9f48c228f0930497e9046e26d9d0749c
crazyrems <tauziac0remy+fork@gmail.com>
2020-04-19 12:11:36 +0200

    New model and animations

--- Enemies/Scorpion/Scorpion.gd
+++ Enemies/Scorpion/Scorpion.gd
@@ -16,7 +16,8 @@ var state = ScorpionState.moveRight
 var nextChoice = 2
 
 func _ready():
-	randomize()
+	rdmz.seed = OS.get_system_time_secs()
+	randomizeChoice()
 
 func hit(hitDamage: float):
 	if isAlive():
@@ -26,44 +27,51 @@ func hit(hitDamage: float):
 	
 	if !isAlive():
 		print("Scorpion is dead")
+		$"MainCollisionShape".disabled = true
+
+func randomizeChoice():
+	nextChoice = rdmz.randf_range(0.1, 0.8)
+	match rdmz.randi_range(0, 7):
+		0, 1:
+			state = ScorpionState.moveLeft
+		2, 3: 
+			state = ScorpionState.moveRight
+		4: 
+			state = ScorpionState.moveForward
+		5: 
+			state = ScorpionState.moveBackward
+		6: 
+			state = ScorpionState.attack
+			nextChoice = 0.2
+		7: 
+			state = ScorpionState.dodgeBack
+			nextChoice = 0.2
 		
 func _process(delta):
 	if isAlive():
 		nextChoice -= delta
 		if nextChoice < 0:
-			nextChoice = rdmz.randf_range(0.5, 3)		
-			match rdmz.randi_range(0, 5):
-				0:
-					state = ScorpionState.moveLeft
-				1: 
-					state = ScorpionState.moveRight
-				2: 
-					state = ScorpionState.moveForward
-				3: 
-					state = ScorpionState.moveBackward
-				4: 
-					state = ScorpionState.attack
-					nextChoice = 0.8
-				5: 
-					state = ScorpionState.dodgeBack
-					nextChoice = 0.8
+			randomizeChoice()
 	
 		var player: KinematicBody = get_parent().get_parent().find_node("Player")
-		look_at(player.translation, Vector3(0, 1, 0))
+		self.translation
 		match state:
 			ScorpionState.moveLeft:
 				move_and_slide(flattenVector(-global_transform.basis.x) * speed * delta, Vector3(0, 1, 0))
+				look_at(player.translation, Vector3(0, 1, 0))				
 			ScorpionState.moveRight:
 				move_and_slide(flattenVector(global_transform.basis.x) * speed * delta, Vector3(0, 1, 0))
+				look_at(player.translation, Vector3(0, 1, 0))				
 			ScorpionState.moveForward:
 				move_and_slide(flattenVector(global_transform.basis.z) * speed * delta, Vector3(0, 1, 0))
+				look_at(player.translation, Vector3(0, 1, 0))				
 			ScorpionState.moveBackward:
 				move_and_slide(flattenVector(-global_transform.basis.z) * speed * delta, Vector3(0, 1, 0))
+				look_at(player.translation, Vector3(0, 1, 0))				
 			ScorpionState.attack:
-				move_and_slide(flattenVector(global_transform.basis.z) * speed * 3 * delta, Vector3(0, 1, 0))
+				move_and_slide(flattenVector(global_transform.basis.z) * speed * 4 * delta, Vector3(0, 1, 0))
 			ScorpionState.dodgeBack:
-				move_and_slide(flattenVector(-global_transform.basis.z) * speed * 3 * delta, Vector3(0, 1, 0))
-				
+				move_and_slide(flattenVector(-global_transform.basis.z) * speed * 4 * delta, Vector3(0, 1, 0))
 		move_and_slide(Vector3(0, -1, 0))
 
 func flattenVector(vector: Vector3):
